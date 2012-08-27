9b05c560dc5c637a3819728b1c4ad2ed6c502c41
Andrew <arasmussen@katworks.com>
2012-08-27 03:29:50 +0000

    Initial Commit

---
+++ root/index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Andrew Rasmussen</title>
+    <link rel="stylesheet" href="/styles.css" />
+  </head>
+  <body>
+    <div id="center">
+      <div id="header">
+      </div>
+      <div id="body">
+        <div id="nav">
+          <ul>
+            <li>
+              <img src="https://www.facebook.com/favicon.ico" />
+              <a href="https://www.facebook.com/rasmussen.andrew">
+                facebook
+              </a>
+            </li>
+            <li>
+              <img src="http://stackoverflow.com/favicon.ico" />
+              <a href="http://stackoverflow.com/users/710360/arasmussen">
+                stackoverflow
+              </a>
+            </li>
+            <li>
+              <img src="http://linkedin.com/favicon.ico" />
+              <a href="http://ca.linkedin.com/pub/andrew-rasmussen/18/95/21b">
+                linkedin
+              </a>
+            </li>
+            <li>
+              <img src="https://twitter.com/favicon.ico" />
+              <a href="https://twitter.com/rasmuzen">
+                twitter
+              </a>
+            </li>
+            <li>
+              <img src="https://github.com/favicon.ico" />
+              <a href="https://github.com/arasmussen">
+                github
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div id="footer">
+      </div>
+    </div>
+  </body>
+</html>
