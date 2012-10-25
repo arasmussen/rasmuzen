a8f67097b48952fac60fa1812f2f991ee575005c
Andrew <arasmussen@katworks.com>
2012-10-25 22:31:11 +0000

    adding rainbow js stuff

--- root/blog.php
+++ root/blog.php
@@ -1,3 +1,11 @@
-<?php
-
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Test blog post</title>
+    <link href="/css/theme.css" rel="stylesheet" type="text/css">
+    <script src="/js/custom-rainbow.min.js"></script>
+  </head>
+  <body>
+<?php require_once('../blog/mysql_on_cygwin.php'); ?>
+  </body>
+</html>
