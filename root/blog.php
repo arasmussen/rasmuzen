65b85c081eee09ff01af6fbc38a65051ee0a3df5
Andrew <arasmussen@katworks.com>
2012-10-26 06:09:19 +0000

    moved header/footer into separate view files

--- root/blog.php
+++ root/blog.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Test blog post</title>
-    <link href="/css/theme.css" rel="stylesheet" type="text/css">
-    <script src="/js/rainbow-custom.min.js"></script>
-  </head>
-  <body>
-<?php require_once('../blog/mysql_on_cygwin.html'); ?>
-  </body>
-</html>
+<?php
+
+require_once('../views/header.php');
+require_once('../blog/mysql_on_cygwin.html');
+require_once('../views/footer.php');
+
+?>
--- root/index.php
+++ root/index.php
@@ -1,79 +1,4 @@
-<!doctype html>
-<html>
-  <head>
-    <title>Andrew Rasmussen</title>
-    <link rel="stylesheet" href="/css/reset.css" />
-    <link rel="stylesheet" href="/css/styles.css" />
-    <link rel="stylesheet" href="/css/smoothness/jquery-ui-1.9.0.custom.min.css" />
-    <script src="/js/jquery-1.8.2.js"></script>
-    <script src="/js/jquery-ui-1.9.0.custom.min.js"></script>
-    <script type="text/javascript">
-      $(function() {
-        $('#accordion').accordion({
-          heightStyle: "content"
-        });
-        $('#tabs').tabs();
-      });
-    </script>
-    <script type="text/javascript">
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-34368328-1']);
-      _gaq.push(['_trackPageview']);
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-    </script>
-  </head>
-  <body>
-    <div id="center">
-      <div id="left">
-        <div id="profile">
-          <img src="/img/andrew.png" alt="andrew" />
-        </div>
-        <div id="nav">
-          <ul>
-<?php
-
-$domains = array(
-  'facebook' => 'https://www.facebook.com',
-  'stackoverflow' => 'http://stackoverflow.com',
-  'linkedin' => 'http://linkedin.com',
-  'twitter' => 'https://twitter.com',
-  'github' => 'https://github.com',
-  'resume' => 'http://www.rasmuzen.com'
-);
-$profiles = array(
-  'facebook' => '/rasmussen.andrew',
-  'stackoverflow' => '/users/710360/arasmussen',
-  'linkedin' => '/pub/andrew-rasmussen/18/95/21b',
-  'twitter' => '/rasmuzen',
-  'github' => '/arasmussen',
-  'resume' => '/resume.php'
-);
-$icons = array(
-  'facebook' => '/img/icons/facebook.png',
-  'stackoverflow' => '/img/icons/stackoverflow.png',
-  'linkedin' => '/img/icons/linkedin.png',
-  'twitter' => '/img/icons/twitter.png',
-  'github' => '/img/icons/github.png',
-  'resume' => ' /img/icons/rasmuzen.png'
-);
-
-foreach ($domains as $site => $domain) {
-  echo '<li>';
-  echo   '<a href="' . $domain . $profiles[$site] . '">';
-  echo     '<img class="navImg" src="' . $icons[$site] . '" alt="' . $site . ' icon" />';
-  echo     '<span class="navSite">' . $site . '</span>';
-  echo   '</a>';
-  echo '</li>';
-}
-?>
-          </ul>
-        </div>
-      </div>
-      <div id="content">
+<?php require_once('../views/header.php'); ?>
         <h1>Andrew Rasmussen</h1>
         <h2>arasmussen (at) katworks (dot) com</h2>
         <div id="accordion">
@@ -138,9 +63,5 @@ foreach ($domains as $site => $domain) {
             </div>
           </div>
         </div>
-      </div>
-      <div id="footer">
-      </div>
-    </div>
-  </body>
-</html>
+
+<?php require_once('../views/footer.php'); ?>
